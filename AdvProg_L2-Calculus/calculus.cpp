c842e381352090a902e5d3101f91057f9af1fb6e
luongthihao <luongthihao2810@gmail.com>
2023-02-24 10:48:45 +0700

    caculus2

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -45,9 +45,9 @@ double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
-      return sqrt(x);
     }
+    return sqrt(x);
 
 
-    return 0;
+
 }
